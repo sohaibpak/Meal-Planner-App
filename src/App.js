181bbc1e9e54bf61e82d996dcaa829e31778070b
Sohaib <sohaibsarwar098@gmail.com>
2025-01-20 17:18:46 +0500

    Update Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import LoginForm from "./pages/LoginForm";
+import LoginForm from "./pages/LoginForm";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/loginform" element={<LoginForm />} /> */}
+        <Route path="/loginform" element={<LoginForm />} />
       </Routes>
     </Router>
   );
